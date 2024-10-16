3cfa656f5c603aae1c7e801a8b5f7461814f278f
SZBS03 <zaidbinshams03@gmail.com>
2024-10-16 16:35:43 +0500

    Dram fixed removing reset pin

--- single_cycle_dev/DRAM_4x72/src/dram.v
+++ single_cycle_dev/DRAM_4x72/src/dram.v
@@ -1,6 +1,5 @@
 module DRAM(
     input wire clk,
-    input wire reset,
     input [1:0]address,
     input [71:0]dataIN,
     output reg [71:0]dataOUT,
@@ -11,17 +10,11 @@ reg [71:0] DRAM [3:0];
 integer i;
 
 always @(posedge clk) begin
-    if(reset) begin
-        for (i=0;i<4;i++)
-        DRAM[i] = 0;
-    end
-    else begin 
     if(Write_ReadCOMP) begin 
         DRAM[address] <= dataIN;
     end
     else if(~Write_ReadCOMP) begin 
         dataOUT <= DRAM[address];
-    end
     end
 end
 endmodule
--- single_cycle_dev/DRAM_4x72/tb/dram_tb.v
+++ single_cycle_dev/DRAM_4x72/tb/dram_tb.v
@@ -1,6 +1,5 @@
 module DRAM_tb();
     reg clk;
-    reg reset;
     reg [1:0]address;
     reg [71:0]dataIN;
     wire [71:0]dataOUT;
@@ -8,7 +7,6 @@ module DRAM_tb();
 
 DRAM u_DRAM(
     .clk(clk),
-    .reset(reset),
     .address(address),
     .dataIN(dataIN),
     .dataOUT(dataOUT),
@@ -22,14 +20,10 @@ end
 
 initial begin
     clk = 0;
-    reset = 1;
     address = 2'b10;
     dataIN = 72'd12;
-    Write_ReadCOMP=0;
-    #20; 
-    reset = 0;
     #5;
-
+    Write_ReadCOMP=0;
     #5;
     Write_ReadCOMP=1;
     #10;
